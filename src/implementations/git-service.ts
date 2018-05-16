b40d65194f2f55bd011d2fc5f5541768cca50f2d
Randy Ehret <randyehret@gmail.com>
2018-05-16 11:01:02 -0500

    Add unit tests for GitService

--- src/implementations/git-service.ts
+++ src/implementations/git-service.ts
@@ -23,9 +23,9 @@ export class GitService implements IGitService {
 			git.clone(repoUrl, options as string[], (err) => {
 				if (err) {
 					reject(err);
+				} else {
+					resolve(`${destination}/${repoSetting.repo}.git`);
 				}
-
-				resolve(`${destination}/${repoSetting.repo}.git`);
 			});
 		});
 	}
@@ -36,22 +36,22 @@ export class GitService implements IGitService {
 			git.tags((err, tags) => {
 				if (err) {
 					reject(err);
-				}
-
-				const semverRegex = /^\d+\.\d+\.\d+$/;
-				if (semverRegex.test(tags.latest)) {
-					resolve(tags.latest);
 				} else {
-					let semverTagFound = false;
-					for (let i = tags.all.length - 1; i >= 0 && !semverTagFound; i--) {
-						if (semverRegex.test(tags.all[i])) {
-							semverTagFound = true;
-							resolve(tags.all[i]);
+					const semverRegex = /^\d+\.\d+\.\d+$/;
+					if (semverRegex.test(tags.latest)) {
+						resolve(tags.latest);
+					} else {
+						let semverTagFound = false;
+						for (let i = tags.all.length - 1; i >= 0 && !semverTagFound; i--) {
+							if (semverRegex.test(tags.all[i])) {
+								semverTagFound = true;
+								resolve(tags.all[i]);
+							}
 						}
-					}
 
-					if (!semverTagFound) {
-						resolve('');
+						if (!semverTagFound) {
+							resolve('');
+						}
 					}
 				}
 			});
@@ -60,18 +60,22 @@ export class GitService implements IGitService {
 
 	public HasChanges(repoFolderPath: string, repoSetting: RepoSetting): Promise<boolean> {
 		return new Promise(async (resolve, reject) => {
-			const latestTag = await this.GetLatestTag(repoFolderPath);
-			if (latestTag.trim().length === 0) {
-				resolve(false);
-			} else {
-				const git = this.git(repoFolderPath);
-				git.log(['--merges', `${repoSetting.base}...${latestTag}`], (err, commits) => {
-					if (err) {
-						reject(err);
-					}
-
-					resolve(commits.total > 0);
-				});
+			try {
+				const latestTag = await this.GetLatestTag(repoFolderPath);
+				if (latestTag.trim().length === 0) {
+					resolve(false);
+				} else {
+					const git = this.git(repoFolderPath);
+					git.log(['--merges', `${repoSetting.base}...${latestTag}`], (err, commits) => {
+						if (err) {
+							reject(err);
+						} else {
+							resolve(commits.total > 0);
+						}
+					});
+				}
+			} catch (ex) {
+				reject(ex);
 			}
 		});
 	}

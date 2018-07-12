2c75d6701fb907b22a710b09877b22747a00b818
Randy Ehret <randyehret@gmail.com>
2018-07-12 17:12:52 -0500

    Add bulk of ProgramProvider tests

--- spec/implementations/program-provider.spec.ts
+++ spec/implementations/program-provider.spec.ts
@@ -1,6 +1,5 @@
-import { expect } from 'chai';
 import { IMock, Mock, Times, It } from 'typemoq';
-import commander from 'commander';
+import colors from 'colors';
 import { ProgramProvider } from '../../src/implementations/program-provider';
 import { ReadlineModule } from '../../types/readline';
 import { FsModule } from '../../types/fs';
@@ -10,155 +9,410 @@ import { ISettingsProvider } from '../../src/interfaces/settings-provider.interf
 import { IGitService } from '../../src/interfaces/git-service.interface';
 
 describe('ProgramProvider', () => {
+	let packageMock: IMock<any>;
+	let consoleMock: IMock<NodeJS.WriteStream>;
+	let readlineMock: IMock<ReadlineModule>;
+	let fsMock: IMock<FsModule>;
+	let pathMock: IMock<PathModule>;
+	let rimrafMock: IMock<RimrafModule>;
+	let settingsMock: IMock<ISettingsProvider>;
+	let gitMock: IMock<IGitService>;
+	let program: ProgramProvider;
+
+	beforeEach(() => {
+		packageMock = Mock.ofType<any>();
+		consoleMock = Mock.ofType<NodeJS.WriteStream>();
+		readlineMock = Mock.ofType<ReadlineModule>();
+		fsMock = Mock.ofType<FsModule>();
+		pathMock = Mock.ofType<PathModule>();
+		rimrafMock = Mock.ofType<RimrafModule>();
+		settingsMock = Mock.ofType<ISettingsProvider>();
+		gitMock = Mock.ofType<IGitService>();
+
+		program = new ProgramProvider(
+			packageMock.object,
+			consoleMock.object,
+			readlineMock.object,
+			fsMock.object,
+			pathMock.object,
+			rimrafMock.object,
+			settingsMock.object,
+			gitMock.object
+		);
+	});
+
 	describe('Init', () => {
-		let packageMock: IMock<any>;
-		let consoleMock: IMock<NodeJS.WriteStream>;
-		let readlineMock: IMock<ReadlineModule>;
-		let fsMock: IMock<FsModule>;
-		let pathMock: IMock<PathModule>;
-		let rimrafMock: IMock<RimrafModule>;
-		let settingsMock: IMock<ISettingsProvider>;
-		let gitMock: IMock<IGitService>;
-		let program: ProgramProvider;
+		it('should perform initial setup on first run', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
 
-		beforeEach(() => {
-			packageMock = Mock.ofType<any>();
-			consoleMock = Mock.ofType<NodeJS.WriteStream>();
-			readlineMock = Mock.ofType<ReadlineModule>();
-			fsMock = Mock.ofType<FsModule>();
-			pathMock = Mock.ofType<PathModule>();
-			rimrafMock = Mock.ofType<RimrafModule>();
-			settingsMock = Mock.ofType<ISettingsProvider>();
-			gitMock = Mock.ofType<IGitService>();
-
-			program = new ProgramProvider(
-				packageMock.object,
-				consoleMock.object,
-				readlineMock.object,
-				fsMock.object,
-				pathMock.object,
-				rimrafMock.object,
-				settingsMock.object,
-				gitMock.object
-			);
-		});
-
-		it('should return an instance of Command', () => {
 			// Act
-			const result = program.Init();
+			await program.Init();
 
 			// Assert
-			expect(result).to.be.an.instanceOf(commander.Command);
+			settingsMock.verify((x) => x.Init(), Times.once());
 		});
 
-		describe('"init" command', () => {
-			it('should perform initial setup on first run', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
-				const command = program.Init();
+		it('should not perform initial setup on subsequent runs', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
 
-				// Act
-				command.parse(['', '', 'init']);
+			// Act
+			await program.Init();
 
-				// Assert
-				settingsMock.verify((x) => x.Init(), Times.once());
-			});
-
-			it('should not perform initial setup on subsequent runs', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
-				const command = program.Init();
-
-				// Act
-				command.parse(['', '', 'init']);
-
-				// Assert
-				settingsMock.verify((x) => x.Init(), Times.never());
-			});
-
-			it('should perform initial setup if "--force" flag is supplied', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
-				const command = program.Init();
-
-				// Act
-				command.parse(['', '', 'init', '--force']);
-
-				// Assert
-				settingsMock.verify((x) => x.Init(), Times.once());
-			});
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.never());
 		});
 
-		describe('"list" command', () => {
-			const repos: RepoSetting[] = [
+		it('should perform initial setup if "force" parameter is truthy', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+
+			// Act
+			await program.Init(true);
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.once());
+		});
+	});
+
+	describe('List', () => {
+		const repos: RepoSetting[] = [
+			{
+				user: 'user1',
+				repo: 'repo1',
+				base: 'base1'
+			},
+			{
+				user: 'user1',
+				repo: 'repo2',
+				base: 'base2'
+			},
+			{
+				user: 'user2',
+				repo: 'repo3',
+				base: 'base3'
+			}
+		];
+
+		it('should list all repositories', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
+
+			// Act
+			await program.List();
+
+			// Assert
+			consoleMock.verify((x) => x.write(It.isAnyString()), Times.exactly(repos.length));
+		});
+
+		it('should not write to console if there are no repositories', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.GetRepos()).returns(() => []);
+
+			// Act
+			await program.List();
+
+			// Assert
+			consoleMock.verify((x) => x.write(It.isAnyString()), Times.never());
+		});
+
+		it('should initialize settings if not previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
+			settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
+
+			// Act
+			await program.List();
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.once());
+		});
+
+		it('should not initialize settings if previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
+
+			// Act
+			await program.List();
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.never());
+		});
+	});
+
+	describe('Add', () => {
+		it('should add the repo to the configuration file', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.AddRepo(It.isAny())).returns(async () => true);
+			const newRepo: RepoSetting = {
+				user: 'newUser',
+				repo: 'newRepo',
+				base: 'newBase'
+			};
+
+			// Act
+			await program.Add(`${newRepo.user}/${newRepo.repo}`, newRepo.base);
+
+			// Assert
+			settingsMock.verify((x) => {
+				return x.AddRepo(It.is((repo: RepoSetting) => {
+					return repo.user === newRepo.user &&
+						repo.repo === newRepo.repo &&
+						repo.base === newRepo.base;
+				}));
+			}, Times.once());
+		});
+
+		it('should write success message if repo was successfully added', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.AddRepo(It.isAny())).returns(async () => true);
+
+			// Act
+			await program.Add('user/repo', 'base');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Added repo'))
+			), Times.once());
+		});
+
+		it('should write failure message if repo was not successfully added', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.AddRepo(It.isAny())).returns(async () => false);
+
+			// Act
+			await program.Add('user/repo', 'base');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Added repo'))
+			), Times.never());
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Warning'))
+			), Times.once());
+		});
+
+		it('should initialize settings if not previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
+
+			// Act
+			await program.Add('user/repo', 'base');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.once());
+		});
+
+		it('should not initialize settings if previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+
+			// Act
+			await program.Add('user/repo', 'base');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.never());
+		});
+	});
+
+	describe('Update', () => {
+		it('should update the repo in the configuration file', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.UpdateRepo(It.isAny())).returns(async () => true);
+			const newRepo: RepoSetting = {
+				user: 'newUser',
+				repo: 'newRepo',
+				base: 'newBase'
+			};
+
+			// Act
+			await program.Update(`${newRepo.user}/${newRepo.repo}`, newRepo.base);
+
+			// Assert
+			settingsMock.verify((x) => {
+				return x.UpdateRepo(It.is((repo: RepoSetting) => {
+					return repo.user === newRepo.user &&
+						repo.repo === newRepo.repo &&
+						repo.base === newRepo.base;
+				}));
+			}, Times.once());
+		});
+
+		it('should write success message if repo was successfully updated', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.UpdateRepo(It.isAny())).returns(async () => true);
+
+			// Act
+			await program.Update('user/repo', 'base');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Updated repo'))
+			), Times.once());
+		});
+
+		it('should write failure message if repo was not successfully updated', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.UpdateRepo(It.isAny())).returns(async () => false);
+
+			// Act
+			await program.Update('user/repo', 'base');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Updated repo'))
+			), Times.never());
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Warning'))
+			), Times.once());
+		});
+
+		it('should initialize settings if not previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
+
+			// Act
+			await program.Update('user/repo', 'base');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.once());
+		});
+
+		it('should not initialize settings if previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+
+			// Act
+			await program.Update('user/repo', 'base');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.never());
+		});
+	});
+
+	describe('Remove', () => {
+		it('should remove the repo from the configuration file', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.RemoveRepo(It.isAny())).returns(async () => true);
+			const newRepo: RepoSetting = {
+				user: 'newUser',
+				repo: 'newRepo',
+				base: 'newBase'
+			};
+
+			// Act
+			await program.Remove(`${newRepo.user}/${newRepo.repo}`);
+
+			// Assert
+			settingsMock.verify((x) => {
+				return x.RemoveRepo(It.is((repo: RepoSetting) => {
+					return repo.user === newRepo.user &&
+						repo.repo === newRepo.repo;
+				}));
+			}, Times.once());
+		});
+
+		it('should write success message if repo was successfully removed', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.RemoveRepo(It.isAny())).returns(async () => true);
+
+			// Act
+			await program.Remove('user/repo');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Removed repo'))
+			), Times.once());
+		});
+
+		it('should write failure message if repo was not successfully removed', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			settingsMock.setup((x) => x.RemoveRepo(It.isAny())).returns(async () => false);
+
+			// Act
+			await program.Remove('user/repo');
+
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Removed repo'))
+			), Times.never());
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message.includes('Warning'))
+			), Times.once());
+		});
+
+		it('should initialize settings if not previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
+
+			// Act
+			await program.Remove('user/repo');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.once());
+		});
+
+		it('should not initialize settings if previously initialized', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+
+			// Act
+			await program.Remove('user/repo');
+
+			// Assert
+			settingsMock.verify((x) => x.Init(), Times.never());
+		});
+	});
+
+	describe('Execute', () => {
+		it('should list repos with changes', async () => {
+			// Arrange
+			settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
+			fsMock.setup((x) => x.mkdtempSync(It.isAnyString())).returns(() => '/temp');
+			settingsMock.setup((x) => x.GetRepos()).returns(() => [
 				{
 					user: 'user1',
 					repo: 'repo1',
 					base: 'base1'
 				},
 				{
-					user: 'user1',
+					user: 'user2',
 					repo: 'repo2',
 					base: 'base2'
 				},
 				{
-					user: 'user2',
+					user: 'user3',
 					repo: 'repo3',
 					base: 'base3'
 				}
-			];
+			]);
+			gitMock.setup((x) => x.CloneRepo(It.isAny(), It.isAnyString(), It.isAny()))
+				.returns(async () => '/temp');
+			gitMock.setup((x) => x.HasChanges(It.isAnyString(), It.isAny()))
+				.returns(async (_path, repo) => repo.repo === 'repo1');
+			fsMock.setup((x) => x.readdirSync(It.isAny())).returns(() => []);
+			colors.disable();
 
-			it('should list all repositories', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
-				settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
-				const command = program.Init();
+			// Act
+			await program.Execute();
 
-				// Act
-				command.parse(['', '', 'list']);
-
-				// Assert
-				consoleMock.verify((x) => x.write(It.isAnyString()), Times.exactly(repos.length));
-			});
-
-			it('should not write to console if there are no repositories', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
-				settingsMock.setup((x) => x.GetRepos()).returns(() => []);
-				const command = program.Init();
-
-				// Act
-				command.parse(['', '', 'list']);
-
-				// Assert
-				consoleMock.verify((x) => x.write(It.isAnyString()), Times.never());
-			});
-
-			it('should initialize settings if not previously initialized', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => true);
-				settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
-				const command = program.Init();
-
-				// Act
-				command.parse(['', '', 'list']);
-
-				// Assert
-				settingsMock.verify((x) => x.Init(), Times.once());
-			});
-
-			it('should not initialize settings if previously initialized', () => {
-				// Arrange
-				settingsMock.setup((x) => x.IsFirstRun()).returns(() => false);
-				settingsMock.setup((x) => x.GetRepos()).returns(() => repos);
-				const command = program.Init();
-
-				// Act
-				command.parse(['', '', 'list']);
-
-				// Assert
-				settingsMock.verify((x) => x.Init(), Times.never());
-			});
+			// Assert
+			consoleMock.verify((x) => x.write(
+				It.is((message: string) => message === 'user1/repo1\n')
+			), Times.once());
 		});
 	});
 });

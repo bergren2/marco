2f871d913a51e28ff9fef4be51484b27b757d52c
Randy Ehret <randallehret@gmail.com>
2018-07-24 11:11:46 -0500

    Rename programProvider to programService

--- src/index.ts
+++ src/index.ts
@@ -4,48 +4,48 @@ import { container } from './container';
 import { IProgramService } from './interfaces/program-service.interface';
 import { SYMBOLS } from './symbols';
 
-const programProvider = container.get<IProgramService>(SYMBOLS.ProgramService);
+const programService = container.get<IProgramService>(SYMBOLS.ProgramService);
 
 (function configure() {
 	program
 		.option('--no-color', 'Disable colorized output')
-		.version(programProvider.Version, '-v, --version');
+		.version(programService.Version, '-v, --version');
 
 	program
 		.command('list')
 		.description('List all repositories')
-		.action(async () => await programProvider.List());
+		.action(async () => await programService.List());
 
 	program
 		.command('add <user>/<repo> [base]')
 		.description('Add a repository')
-		.action(async (repoArg: string, base: string) => await programProvider.Add(repoArg, base));
+		.action(async (repoArg: string, base: string) => await programService.Add(repoArg, base));
 
 	program
 		.command('update <user>/<repo> <base>')
 		.description('Update a repository\'s base branch')
-		.action(async (repoArg: string, base: string) => await programProvider.Update(repoArg, base));
+		.action(async (repoArg: string, base: string) => await programService.Update(repoArg, base));
 
 	program
 		.command('remove <user>/<repo>')
 		.description('Remove a repository')
-		.action(async (repoArg: string) => await programProvider.Remove(repoArg));
+		.action(async (repoArg: string) => await programService.Remove(repoArg));
 
 	program
 		.command('import <config>')
 		.description('Replace configuration with config from stdin')
-		.action(async (repoJson: string) => await programProvider.Import(repoJson));
+		.action(async (repoJson: string) => await programService.Import(repoJson));
 
 	program
 		.command('export')
 		.description('Write configuration to stdout')
 		.option('-p, --pretty', 'Pretty-print config')
-		.action(async (args: any) => await programProvider.Export(args.pretty));
+		.action(async (args: any) => await programService.Export(args.pretty));
 
 	program
 		.command('polo')
 		.description('Fetches release information')
-		.action(async () => await programProvider.Execute());
+		.action(async () => await programService.Execute());
 })();
 
 export default program;
